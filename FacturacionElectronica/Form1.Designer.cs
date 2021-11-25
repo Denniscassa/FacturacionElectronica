607ca5bed90018482c5717b7a66ca3d61c5b7e51
YONDER Q <134538@unsaac.edu.pe>
2021-11-25 12:31:18 -0500

    abrir exel
    abrir ecel

--- FacturacionElectronica/Form1.Designer.cs
+++ FacturacionElectronica/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace FacturacionElectronica
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 308);
+            this.ClientSize = new System.Drawing.Size(658, 369);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace FacturacionElectronica
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FacturacionElectronica/Form1.cs
+++ FacturacionElectronica/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpreadsheetLight;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,41 @@ namespace FacturacionElectronica
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // abrir exel
+            String path = @"";
+
+            // abrir archivo
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                path = path + sFileName;
+                Console.WriteLine(sFileName);
+            }
+
+            // leer archivo
+             int inicio = 2;
+             SLDocument doc = new SLDocument(path);
+             while (!string.IsNullOrEmpty(doc.GetCellValueAsString(inicio, 1)))
+             {
+                 int codigo = doc.GetCellValueAsInt32(inicio, 1);
+                 String nombre = doc.GetCellValueAsString(inicio,2);
+                 int edad = doc.GetCellValueAsInt32(inicio,3);
+                 inicio++;
+                 Console.WriteLine("Codigo: "+codigo.ToString()+"Nombre: "+ nombre+ "Edad: "+ edad.ToString());
+
+             }
+             //escribir en la base de datos
+             
+            
         }
     }
 }
